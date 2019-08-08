709909f4b405717aaa513aec6d5e1c775e5aa99f
myy <m973858776@163.com>
2019-08-08 15:28:35 +0800

    comm

--- app/src/main/java/com/example/ergedd_android2/MainActivity.java
+++ app/src/main/java/com/example/ergedd_android2/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.ergedd_android2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//项目经理：马屹延
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+    }
+
+    private void initview() {
+
+        
     }
 }
