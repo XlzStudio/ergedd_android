46123981dd1ae4addd0ad782ba0bf5d06c949578
lzk6595 <914362278@qq.com>
2019-08-09 14:23:17 +0800

    Merge branch 'master' of https://github.com/myy1128/ergedd_android
    # Conflicts:
    #	app/src/main/java/com/example/ergedd_android2/MainActivity.java
    #	app/src/main/java/com/example/ergedd_android2/utils/HttpManager.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/ergedd_android2/bean/BabyLookTabBean.java
+++ app/src/main/java/com/example/ergedd_android2/bean/BabyLookTabBean.java
@@ -1,5 +1,216 @@
 package com.example.ergedd_android2.bean;
 
+import java.util.List;
+
 public class BabyLookTabBean {
 
+    /**
+     * id : 4
+     * name : 动画
+     * display_count : 4
+     * copyright_sensitive : 1
+     * albums : [{"id":646,"name":"萌鸡小队 第二季","image_url":"http://img5g22.ergedd.com/album/646_1550054329322.jpg","description":"启迪小萌鸡，欢乐再延续！","status":1,"video_count":52,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"奥飞文化-奥飞"},{"id":645,"name":"超级飞侠 第五季","image_url":"http://img5g22.ergedd.com/album/645_1550054122266.jpg","description":"新成员 新团队 集结完毕！","status":1,"video_count":20,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"奥飞文化-奥飞"},{"id":563,"name":"小马宝莉 第七季","image_url":"http://img5g22.ergedd.com/album/563_1537946105629.jpg","description":"小马们迎来新挑战","status":1,"video_count":26,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"孩之宝"},{"id":175,"name":"汪汪队立大功","image_url":"http://img5g22.ergedd.com/album/175_1550109910036.jpg","description":"与狗狗一起化解危机","status":1,"video_count":96,"play_count":339109,"icon_url":"http://img5g22.ergedd.com/album/175_1506333512989.png","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"http://img5g22.ergedd.com/album/175_1544175315949.png","publisher_name":"戎音广告"},{"id":33,"name":"小猪佩奇","image_url":"http://img5g22.ergedd.com/album/33_1500456911360.png","description":"粉红猪小妹的情商课","status":1,"video_count":156,"play_count":166123434,"icon_url":"http://img5g22.ergedd.com/album/33_1493977102106.png","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"http://img5g22.ergedd.com/album/33_1544176577740.png","publisher_name":"山成十趣-小猪佩奇"},{"id":659,"name":"爱探险的朵拉","image_url":"http://img5g22.ergedd.com/album/36118302384_1553502781888.jpg","description":"风靡全球的英语教学片","status":1,"video_count":40,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/659_1554979285069.png","index_recommend":1,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"戎音广告"},{"id":649,"name":"睡衣小英雄","image_url":"http://img5g22.ergedd.com/album/77218374819_1550214348117.jpg","description":"夜幕下的超级英雄","status":1,"video_count":52,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/649_1561463603511.png","index_recommend":1,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"森宇文化"},{"id":648,"name":"宇宙护卫队","image_url":"http://img5g22.ergedd.com/album/72864083045_1550214165782.jpg","description":"勇敢出发 一同保护动物","status":1,"video_count":52,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"完美鲲鹏"},{"id":707,"name":"西游记的故事","image_url":"http://img5g22.ergedd.com/album/88144171762_1559810212095.jpg","description":"经典名著改编 超萌孙悟空","status":1,"video_count":52,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"金丁美奇"},{"id":532,"name":"萌鸡小队","image_url":"http://img5g22.ergedd.com/album/532_1537497054130.jpg","description":"社交启蒙爆款动画","status":1,"video_count":104,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/532_1537872620253.png","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"http://img5g22.ergedd.com/album/532_1544177690083.png","publisher_name":"奥飞文化-奥飞"},{"id":514,"name":"海底小纵队","image_url":"http://img5g22.ergedd.com/album/514_1531906089603.jpg","description":"海底世界大冒险","status":1,"video_count":26,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/514_1548125393319.png","index_recommend":1,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"万达儿童-海底小纵队"},{"id":728,"name":"大都会小企鹅","image_url":"http://img5g22.ergedd.com/album/28412683376_1563257640564.jpg","description":"企鹅家族的城市新生活","status":1,"video_count":26,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":1,"is_vip":2,"type":1,"sensitive":8,"erge_img_url":"","publisher_name":"自选测试"},{"id":729,"name":"螺丝钉","image_url":"http://img5g22.ergedd.com/album/94977114333_1563264456063.jpg","description":"神奇螺丝钉带你解决生活难题","status":1,"video_count":15,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":1,"is_vip":2,"type":1,"sensitive":8,"erge_img_url":"","publisher_name":"自选测试"},{"id":730,"name":"动物兄弟","image_url":"http://img5g22.ergedd.com/album/20907256888_1563358977999.jpg","description":"快来一场丛林探险吧","status":1,"video_count":15,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":1,"is_vip":2,"type":1,"sensitive":8,"erge_img_url":"","publisher_name":"自选测试"},{"id":656,"name":"变形警车珀利交通安全篇","image_url":"http://img5g22.ergedd.com/album/3334211230_1553148420649.jpg","description":"生动形象展示交通规则","status":1,"video_count":25,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"乐彩文化"},{"id":677,"name":"天线宝宝","image_url":"http://img5g22.ergedd.com/album/677_1554865452832.jpg","description":"家喻户晓的明星娃娃","status":1,"video_count":120,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/677_1561463580739.png","index_recommend":1,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"DHX"},{"id":233,"name":"小马宝莉","image_url":"http://img5g22.ergedd.com/album/233_1496835226736.jpg","description":"友谊的魔法","status":1,"video_count":65,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/233_1547201586206.png","index_recommend":1,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"孩之宝"},{"id":663,"name":"嗨！道奇","image_url":"http://img5g22.ergedd.com/album/663_1555483225051.jpg","description":"关于成长和友谊的故事","status":1,"video_count":51,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/663_1554369595851.png","index_recommend":1,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"BBC"},{"id":664,"name":"莎拉与乖乖鸭","image_url":"http://img5g22.ergedd.com/album/20798374131_1553851044271.jpg","description":"小屁孩的学前历险记","status":1,"video_count":40,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"BBC"},{"id":562,"name":"超级飞侠","image_url":"http://img5g22.ergedd.com/album/562_1545043168054.jpg","description":"跟着乐迪环游世界","status":1,"video_count":26,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/562_1537873619600.png","index_recommend":1,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"http://img5g22.ergedd.com/album/562_1544176901596.png","publisher_name":"奥飞文化-奥飞"},{"id":565,"name":"小马宝莉大电影","image_url":"http://img5g22.ergedd.com/album/24782205007_1537952354258.jpg","description":"来自人类世界的紫悦","status":1,"video_count":6,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"孩之宝"},{"id":622,"name":"雨果带你看世界","image_url":"http://img5g22.ergedd.com/album/622_1547635611411.jpg","description":"一起去探索哲学世界","status":1,"video_count":52,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"幻维数码"},{"id":665,"name":"小恐龙乐园","image_url":"http://img5g22.ergedd.com/album/665_1554283356331.jpg","description":"恐龙乐园的温馨故事","status":1,"video_count":50,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"BBC"},{"id":676,"name":"比得兔","image_url":"http://img5g22.ergedd.com/album/99584096632_1554271398980.jpg","description":"百年经典IP改编动画","status":1,"video_count":107,"play_count":0,"icon_url":"http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg","index_recommend":null,"copyright_sensitive":0,"is_vip":2,"type":1,"sensitive":0,"erge_img_url":"","publisher_name":"万达儿童-海底小纵队"}]
+     */
+
+    private int id;
+    private String name;
+    private int display_count;
+    private int copyright_sensitive;
+    private List<AlbumsBean> albums;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDisplay_count() {
+        return display_count;
+    }
+
+    public void setDisplay_count(int display_count) {
+        this.display_count = display_count;
+    }
+
+    public int getCopyright_sensitive() {
+        return copyright_sensitive;
+    }
+
+    public void setCopyright_sensitive(int copyright_sensitive) {
+        this.copyright_sensitive = copyright_sensitive;
+    }
+
+    public List<AlbumsBean> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<AlbumsBean> albums) {
+        this.albums = albums;
+    }
+
+    public static class AlbumsBean {
+        /**
+         * id : 646
+         * name : 萌鸡小队 第二季
+         * image_url : http://img5g22.ergedd.com/album/646_1550054329322.jpg
+         * description : 启迪小萌鸡，欢乐再延续！
+         * status : 1
+         * video_count : 52
+         * play_count : 0
+         * icon_url : http://img5g22.ergedd.com/album/64493018054_1492738377273.jpg
+         * index_recommend : null
+         * copyright_sensitive : 0
+         * is_vip : 2
+         * type : 1
+         * sensitive : 0
+         * erge_img_url :
+         * publisher_name : 奥飞文化-奥飞
+         */
+
+        private int id;
+        private String name;
+        private String image_url;
+        private String description;
+        private int status;
+        private int video_count;
+        private int play_count;
+        private String icon_url;
+        private Object index_recommend;
+        private int copyright_sensitive;
+        private int is_vip;
+        private int type;
+        private int sensitive;
+        private String erge_img_url;
+        private String publisher_name;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getImage_url() {
+            return image_url;
+        }
+
+        public void setImage_url(String image_url) {
+            this.image_url = image_url;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+        public int getVideo_count() {
+            return video_count;
+        }
+
+        public void setVideo_count(int video_count) {
+            this.video_count = video_count;
+        }
+
+        public int getPlay_count() {
+            return play_count;
+        }
+
+        public void setPlay_count(int play_count) {
+            this.play_count = play_count;
+        }
+
+        public String getIcon_url() {
+            return icon_url;
+        }
+
+        public void setIcon_url(String icon_url) {
+            this.icon_url = icon_url;
+        }
+
+        public Object getIndex_recommend() {
+            return index_recommend;
+        }
+
+        public void setIndex_recommend(Object index_recommend) {
+            this.index_recommend = index_recommend;
+        }
+
+        public int getCopyright_sensitive() {
+            return copyright_sensitive;
+        }
+
+        public void setCopyright_sensitive(int copyright_sensitive) {
+            this.copyright_sensitive = copyright_sensitive;
+        }
+
+        public int getIs_vip() {
+            return is_vip;
+        }
+
+        public void setIs_vip(int is_vip) {
+            this.is_vip = is_vip;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public void setType(int type) {
+            this.type = type;
+        }
+
+        public int getSensitive() {
+            return sensitive;
+        }
+
+        public void setSensitive(int sensitive) {
+            this.sensitive = sensitive;
+        }
+
+        public String getErge_img_url() {
+            return erge_img_url;
+        }
+
+        public void setErge_img_url(String erge_img_url) {
+            this.erge_img_url = erge_img_url;
+        }
+
+        public String getPublisher_name() {
+            return publisher_name;
+        }
+
+        public void setPublisher_name(String publisher_name) {
+            this.publisher_name = publisher_name;
+        }
+    }
 }
